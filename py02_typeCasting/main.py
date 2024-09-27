f0e7c5109e557de977efb33dcf58961671a7e3d2
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-09-26 21:23:19 -0400

    python 102 - type casting

---
+++ py02_typeCasting/main.py
@@ -0,0 +1,27 @@
+# String str()
+# Integer int()
+# Boolean bool()
+# Float float()
+
+name = "Titann";
+age = 5;
+weight = 38.5;
+is_doggo = True;
+
+print(type(name));
+print(type(age));
+print(type(weight));
+print(type(is_doggo));
+
+age = float(age);
+print(type(age));
+
+weight = int(weight);
+print(type(weight));
+
+is_doggo = str(is_doggo);
+print(is_doggo)
+print(type(is_doggo));
+
+age = bool(age);
+print(age)
