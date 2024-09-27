2bdfeeb89381fe1a67cb7aea4a2ad1909a01a144
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-09-26 21:47:28 -0400

    python 103 - user input

---
+++ py03_userInput/main.py
@@ -0,0 +1,17 @@
+# name = input("Enter your name: ");
+# age = int(input("Enter your age : ")); # Will get String value even input number. Need type casting before calculate
+# age = age + 1;
+#
+# print(f"Hello {name}");
+# print(f"You are {age} years old");
+
+adjective1  = input("Enter an adjective : ");
+noun = input("Enter a noun : ");
+adjective2 = input("Enter an adjective : ");
+verb = input("Enter a verb : ");
+adjective3 = input("Enter an adjectinve : ");
+
+print(f"Today I went to a {adjective1} zoo");
+print(f"In an exhibit, I saw {noun}")
+print(f"{noun} was {adjective2} and {verb}ing")
+print(f"I was {adjective3}")
