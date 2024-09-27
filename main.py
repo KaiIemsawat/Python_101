7081566312e6853766b37584af86e92811acf9c9
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-09-26 20:54:41 -0400

    python 101 - variable

---
+++ main.py
@@ -0,0 +1,32 @@
+# String
+first_name = "Zukkii";
+food = "Human food"
+email = "zukkii@email.com"
+
+print(first_name);
+print(f"Hello {first_name}");
+print(f"You like {food}");
+print(f"Your email is {email}");
+
+# Integer
+age = 7
+quantity = 8
+num_of_brother = 2
+
+print(f"You are {age} years old");
+print(f"You have {quantity} balls");
+print(f"You have {num_of_brother} brothers");
+
+# Float
+price = 30.99
+
+print(f"You vitamin price is ${price}")
+
+# Boolean
+
+is_doggo = True;
+
+if is_doggo:
+    print("You are a doggo")
+else:
+    print("You are not a doggo")
