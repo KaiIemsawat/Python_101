67f06f1a2f30a4ce5048b655dd916b9b5323cd41
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-09-30 22:21:18 -0400

    python if else

---
+++ py05_ifElse/main.py
@@ -0,0 +1,9 @@
+
+age = int(input("Enter your age : "));
+
+if age >= 18:
+    print("You are now signed up..");
+elif age < 0:
+    print("Input age must not be less than 0");
+else:
+    print("You must be 18 or older to sign up");
